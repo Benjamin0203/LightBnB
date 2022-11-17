bf67ca7b6d053478ccc61ba26804838cf9ed6463
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-11-17 00:19:16 -0800

    Makes parameterized query to seperate SQL into two different parts.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,17 +6,30 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const args = process.argv.slice(2);
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = ['%${cohortName}%', limit];
 
-pool.query(`
+const queryString = `
 SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT ${args[1] || 5};
-`).then(res => {
-  // console.log(res.rows);
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  });
-}).catch(err => console.error('query error', err.stack));
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values);
+
+// const args = process.argv.slice(2);
+
+// pool.query(`
+// SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = students.cohort_id
+// WHERE cohorts.name LIKE '%${args[0]}%'
+// LIMIT ${args[1] || 5};
+// `).then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   });
+// }).catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,34 @@ const pool = new Pool({
 });
 
 const cohort_month = process.argv[2];
-
-pool.query(`
+const cohorts_name = '${cohort_month';
+const values = [cohorts_name];
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort, count(assistance_requests.*) AS total_assistances
 FROM teachers
 JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${cohort_month || 'JUL02'}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`).then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`);
-  });
-}).catch(err => console.error('query error', err.stack));
+`;
+
+pool.query(queryString, values);
+//-----------------------------------
+// const cohort_month = process.argv[2];
+
+// pool.query(`
+// SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort, count(assistance_requests.*) AS total_assistances
+// FROM teachers
+// JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+// JOIN students ON students.id = assistance_requests.student_id
+// JOIN cohorts ON students.cohort_id = cohorts.id
+// WHERE cohorts.name = '${cohort_month || 'JUL02'}'
+// GROUP BY teachers.name, cohorts.name
+// ORDER BY teachers.name;
+// `).then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.cohort}: ${user.teacher}`);
+//   });
+// }).catch(err => console.error('query error', err.stack));
