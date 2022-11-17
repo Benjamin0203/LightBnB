c3cef14371b5945456948fefa8931324e61a95ef
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-11-16 16:53:17 -0800

    Fixes issues with 01_schema data (num of ids should be less than 3) and adds bunch of data from link to 02_seeds file.

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -4,18 +4,18 @@ VALUES ('Kris', 'kris@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh7
 ('John', 'john@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 
-INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
-VALUES ('title message', 'description message', 'thumbnail photo url', 'cover photo url', 20, 6, 4, 8, 'country', 'street', 'city', 'province', 'post code'),
-('title message', 'description message', 'thumbnail photo url', 'cover photo url', 20, 6, 6, 7, 'country', 'street', 'city', 'province', 'post code'),
-('title message', 'description message', 'thumbnail photo url', 'cover photo url', 20, 0, 5, 6, 'country', 'street', 'city', 'province', 'post code');
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+VALUES (1, 'title message', 'description message', 'thumbnail photo url', 'cover photo url', 20, 6, 4, 8, 'country', 'street', 'city', 'province', 'post code'),
+(2, 'title message', 'description message', 'thumbnail photo url', 'cover photo url', 20, 6, 6, 7, 'country', 'street', 'city', 'province', 'post code'),
+(3, 'title message', 'description message', 'thumbnail photo url', 'cover photo url', 20, 0, 5, 6, 'country', 'street', 'city', 'province', 'post code');
 
 
-INSERT INTO reservations (start_date, end_date)
-VALUES ('2018-09-11', '2018-09-26'),
-('2019-01-04', '2019-02-01'),
-('2021-10-01', '2021-10-14');
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 3, 3),
+('2019-01-04', '2019-02-01', 1, 1),
+('2021-10-01', '2021-10-14', 2, 2);
 
-INSERT INTO property_reviews (rating, message)
-VALUES (3, 'message'),
-(4, 'message'),
-(5, 'message');
+INSERT INTO property_reviews (property_id, guest_id, reservation_id, rating, message)
+VALUES (1, 1, 1, 3, 'message'),
+(1, 1, 1, 1, 'message'),
+(1, 1, 1, 1, 'message');
